2b899fa2a56cb471fa8c27f11e79161df411ace9
Andrew <andrewgeorgemitchell@gmail.com>
2022-05-07 15:38:19 -0700

    Remove unused stuff

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,12 +1,4 @@
-import { makeStyles } from '@material-ui/core/styles';
 import { Lightbox } from 'react-modal-image';
-
-const useStyles = makeStyles(
-  (theme) => ({
-    root: {},
-  }),
-  { name: `MuiExample_Component` },
-);
 
 type ModalProps = {
   handleClose: any;
@@ -14,15 +6,12 @@ type ModalProps = {
   selectedPhoto: any;
 };
 
-const Modal: React.FC<ModalProps> = ({ image, selectedPhoto }) => {
-  console.log(selectedPhoto, image);
-  return (
-    <div>
-      {image.title === selectedPhoto && (
-        <Lightbox large={image.src} alt={image.title} />
-      )}
-    </div>
-  );
-};
+const Modal: React.FC<ModalProps> = ({ image, selectedPhoto }) => (
+  <div>
+    {image.title === selectedPhoto && (
+      <Lightbox large={image.src} alt={image.title} />
+    )}
+  </div>
+);
 
 export default Modal;
