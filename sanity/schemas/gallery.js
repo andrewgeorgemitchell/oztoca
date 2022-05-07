da568f0569cdc05f8675e9e4a7c99de67d59467a
Andrew <andrewgeorgemitchell@gmail.com>
2022-05-07 11:30:06 -0700

    Add oztoca-sanity to sanity/ folder

---
+++ sanity/schemas/gallery.js
@@ -0,0 +1,27 @@
+const GallerySchema = {
+  title: "Gallery",
+  name: "gallery",
+  type: "document",
+  fields: [
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'image',
+    },
+    {
+      title: "Title",
+      name: "title",
+      type: "string",
+      description: "The title of the picture",
+    },
+    {
+      title: "Description",
+      name: "description",
+      type: "array",
+      of: [{ type: "block" }],
+      description: "The description of the picture",
+    },
+  ],
+};
+
+export default GallerySchema;
