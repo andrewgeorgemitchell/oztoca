7f56a4b63faff93962a089c40e6f97c7bca9e53e
Andrew <andrewgeorgemitchell@gmail.com>
2022-05-07 11:32:38 -0700

    Fix sanity/ eslint issues

--- sanity/schemas/category.js
+++ sanity/schemas/category.js
@@ -1,26 +1,26 @@
 const CategorySchema = {
-  title: "Cat Categories",
-  name: "category",
-  type: "document",
+  title: 'Cat Categories',
+  name: 'category',
+  type: 'document',
   fields: [
     {
-      title: "Name",
-      name: "name",
-      type: "string",
+      title: 'Name',
+      name: 'name',
+      type: 'string',
       description: 'Name of the category of cat (e.g. "Stud", "Champion")',
       validation: (R) => R.required(),
     },
     {
-      title: "Slug",
-      name: "slug",
-      type: "slug",
-      description: "The text used for the url, cannot contain spaces",
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      description: 'The text used for the url, cannot contain spaces',
       validation: (R) => R.required(),
       options: {
-        source: "name",
+        source: 'name',
         maxLength: 200, // will be ignored if slugify is set
         slugify: (input) =>
-          input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+          input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
       },
     },
   ],
--- sanity/schemas/gallery.js
+++ sanity/schemas/gallery.js
@@ -1,7 +1,7 @@
 const GallerySchema = {
-  title: "Gallery",
-  name: "gallery",
-  type: "document",
+  title: 'Gallery',
+  name: 'gallery',
+  type: 'document',
   fields: [
     {
       title: 'Image',
@@ -9,17 +9,17 @@ const GallerySchema = {
       type: 'image',
     },
     {
-      title: "Title",
-      name: "title",
-      type: "string",
-      description: "The title of the picture",
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+      description: 'The title of the picture',
     },
     {
-      title: "Description",
-      name: "description",
-      type: "array",
-      of: [{ type: "block" }],
-      description: "The description of the picture",
+      title: 'Description',
+      name: 'description',
+      type: 'array',
+      of: [{ type: 'block' }],
+      description: 'The description of the picture',
     },
   ],
 };
